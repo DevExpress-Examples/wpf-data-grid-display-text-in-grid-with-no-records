8e2843d2c2017369926035b75b1fcd1fb0a93935
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/GridExample/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports System
+Imports System.Windows
+
+Namespace GridExample
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
