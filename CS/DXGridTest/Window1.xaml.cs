62e3575c7a1bced567827414dbcacc2ab08d1a50
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGridTest/Window1.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Utils.Themes;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Themes;
+
+namespace DXGridTest {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public static readonly DependencyProperty IsGridEmptyProperty;
+        static Window1() {
+            IsGridEmptyProperty = DependencyProperty.RegisterAttached("IsGridEmpty", typeof(bool), typeof(Window1), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.Inherits));
+        }
+        public static bool GetIsGridEmpty(DependencyObject dependencyObject) {
+            return (bool)dependencyObject.GetValue(IsGridEmptyProperty);
+        }
+        public static void SetIsGridEmpty(DependencyObject dependencyObject, bool value) {
+            dependencyObject.SetValue(IsGridEmptyProperty, value);
+        }
+        public Window1() {
+            InitializeComponent();
+
+
+            List<TestData> list = new List<TestData>();
+            for(int i = 0; i < 100; i++)
+                list.Add(new TestData() { Text = "Row" + i, Number = i, Group = i % 5 });
+            grid.DataSource = list;
+
+            grid.LayoutUpdated += new EventHandler(grid_LayoutUpdated);
+        }
+
+        void grid_LayoutUpdated(object sender, EventArgs e) {
+            Window1.SetIsGridEmpty(grid, grid.VisibleRowCount == 0);
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) {
+            grid.SetValue(ThemeManager.ThemeNameProperty, "Azure");
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e) {
+            grid.ClearValue(ThemeManager.ThemeNameProperty);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Window1.SetIsGridEmpty(grid, !Window1.GetIsGridEmpty(grid));
+        }
+    }
+
+    public class TestData {
+        public string Text { get; set; }
+        public int Number { get; set; }
+        public int Group { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# How to display a line when the DXGrid doesn't include any record 
+
+
+<p>This sample shows how to display a message in the grid if there are no visible rows.</p>
+
+
+<h3>Description</h3>
+
+<p><br />
+</p>
+
+<br/>
+
+
