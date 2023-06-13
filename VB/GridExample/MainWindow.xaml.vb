9ae070090edad638f8ee4b236036f488bdad7c50
DevExpressExampleBot <fakeEmail>
2023-06-13 11:48:35 +0400

    Source auto update [skip ci]

--- VB/GridExample/MainWindow.xaml.vb
+++ VB/GridExample/MainWindow.xaml.vb
@@ -1,12 +1,12 @@
-﻿Imports System
 Imports System.Windows
 
 Namespace GridExample
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 End Namespace
